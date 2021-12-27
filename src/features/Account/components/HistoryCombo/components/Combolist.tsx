46f8e13bfa9e40ee612da7d32e0bac2efdc15487
Toàn Nguyễn <62341100+ToanNguyenNgoc@users.noreply.github.com>
2021-12-27 17:53:16 +0700

    [UPDATE] reset source

--- src/features/Account/components/HistoryCombo/components/Combolist.tsx
+++
@@ -1,8 +0,0 @@
-function ComboList(props: any) {
-  return (
-    <div className="payment_section text-color-purple">
-      <h1 className="title">Combo</h1>
-    </div>
-  );
-}
-export default ComboList;
