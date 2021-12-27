55d95fa1785daa51a452d9bc81bfa5c04fd4ddc9
Toàn Nguyễn <62341100+ToanNguyenNgoc@users.noreply.github.com>
2021-12-27 11:22:48 +0700

    [UPDATE] rename icon

--- src/features/Home/components/HomeLoggedCalendarAppointmentList.tsx
+++ src/features/Home/components/HomeLoggedCalendarAppointmentList.tsx
@@ -8,6 +8,7 @@ export default function HomeLoggedCalendarAppointmentList(
   props: IAppointmentList
 ) {
   const { datingList } = props;
+  console.log(datingList);
 
   const getStartTimeAndId = () => {
     const result: any = [];
@@ -53,15 +54,52 @@ export default function HomeLoggedCalendarAppointmentList(
   const sortedDatingList = sortDatingList();
   console.log("datingList", datingList);
   console.log("sortedDatingList", sortedDatingList);
+  //console.log(sortedDatingList.flat());
 
-  return datingList.map((item: any, index: number) => {
-    return (
-      <div key={index} className="calendar-appointment__list">
-        {item &&
-          item.map((item: any, i: number) => (
-            <HomeLoggedCalendarAppointmentItem key={i} datingList={item} />
-          ))}
-      </div>
-    );
-  });
+  // const array = sortedDatingList.flat();
+  // console.log(array);
+  // const arrPush: any[] = [];
+  // for(var item of array){
+  //   if(item){
+  //     arrPush.push(item)
+  //   }
+  // }
+  // console.log(arrPush)
+
+
+    // return (
+    //   <div className="calendar-appointment__list">
+    //     {arrPush &&
+    //       arrPush.map((item: any, i: number) => (
+    //         <HomeLoggedCalendarAppointmentItem key={i} datingList={item} />
+    //       ))}
+    //   </div>
+    // );
+  return (
+    <>
+      {
+        sortedDatingList.map((date: any, index: number) => (
+          <div key={index} className="calendar-appointment__list">
+            {
+              date &&
+              date.map((item: any, index: number) => (
+                <HomeLoggedCalendarAppointmentItem key={index} datingList={item} />
+              ))
+            }
+          </div>
+        ))
+      }
+    </>
+  )
+
+  // return sortedDatingList.map((item: any, index: number) => {
+  //   return (
+  //     <div key={index} className="calendar-appointment__list">
+  //       {item &&
+  //         item.map((item: any, i: number) => (
+  //           <HomeLoggedCalendarAppointmentItem key={i} datingList={item} />
+  //         ))}
+  //     </div>
+  //   );
+  // });
 }
