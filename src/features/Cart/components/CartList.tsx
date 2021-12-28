da7b52ef0e71e2c3d950a0c4c349386d1d8d9e9d
Toàn Nguyễn <62341100+ToanNguyenNgoc@users.noreply.github.com>
2021-12-28 15:10:28 +0700

    [UPDATE] cart section

--- src/features/Cart/components/CartList.tsx
+++ src/features/Cart/components/CartList.tsx
@@ -4,15 +4,15 @@ import SectionTitle from '../../SectionTitle';
 import icon from '../../../constants/icon';
 import { AppContext } from '../../../context/AppProvider';
 
-function CartList(props:any) {
-      const {listOrg, chooseOrg, carts, cartByOrgId} = props;
-      const {t} = useContext(AppContext)
+function CartList(props: any) {
+      const { listOrg, chooseOrg, carts, cartByOrgId } = props;
+      const { t } = useContext(AppContext)
       const cartJoin = [];
-      for(var org of listOrg){
+      for (var org of listOrg) {
             // eslint-disable-next-line no-loop-func
-            const arr = carts.cartList.filter((item:any)=> item.org_name === org);
-            const arrJoin={
-                  name:org,
+            const arr = carts.cartList.filter((item: any) => item.org_name === org);
+            const arrJoin = {
+                  name: org,
                   cart: arr
             }
             cartJoin.push(arrJoin);
@@ -36,64 +36,94 @@ function CartList(props:any) {
                                           <SectionTitle
                                                 title={item.name}
                                           />
-                                          <span
-                                                className="flex-row cart-list-item__head"
-                                          >
-                                                <img src={icon.box} alt="" />
-                                                {t('Mer_de.services')}
-                                          </span>
-                                          <div className="flex-row cart-list-item__title">
-                                                <span>{t('Mer_de.services_name')}</span>
-                                                <span>{t('pr.quantity')}</span>
-                                                <span>{t('cart.unit_price')}</span>
-                                                <span>{t('pr.total')}</span>
-                                                <span>{t('cart.option')}</span>
-                                          </div>
-                                          <ul className="flex-column">
-                                                {
-                                                      item.cart.filter((item: any) => item.is_type === 2).map((child: any) => (
-                                                            <CartItem
-                                                                  key={child.cart_id}
-                                                                  cartItem={child}
-                                                                  chooseOrg={chooseOrg}
-                                                            />
-                                                      ))
-                                                }
-                                          </ul>
-                                          <span
-                                                className="flex-row cart-list-item__head"
-                                          >
-                                                <img src={icon.bag} alt="" />
-                                                {t('Mer_de.products')}
-                                          </span>
-                                          <ul className="flex-column">
-                                                {
-                                                      item.cart.filter((item: any) => item.is_type === 1).map((child: any) => (
-                                                            <CartItem
-                                                                  key={child.cart_id}
-                                                                  cartItem={child}
-                                                                  chooseOrg={chooseOrg}
-                                                            />
-                                                      ))
-                                                }
-                                          </ul>
-                                          <span
-                                                className="flex-row cart-list-item__head"
-                                          >
-                                                <img src={icon.bag} alt="" />
-                                                Combos
-                                          </span>
-                                          <ul className="flex-column">
-                                                {
-                                                      item.cart.filter((item: any) => item.is_type === 3).map((child: any) => (
-                                                            <CartItem
-                                                                  key={child.cart_id}
-                                                                  cartItem={child}
-                                                                  chooseOrg={chooseOrg}
-                                                            />
-                                                      ))
-                                                }
-                                          </ul>
+                                          {/* services */}
+                                          {
+                                                item.cart.filter((item: any) => item.is_type === 2).length === 0 ?
+                                                      <div className="flex-row cart-list-item__title">
+                                                            <span>{t('Mer_de.services_name')}</span>
+                                                            <span>{t('pr.quantity')}</span>
+                                                            <span>{t('cart.unit_price')}</span>
+                                                            <span>{t('pr.total')}</span>
+                                                            <span>{t('cart.option')}</span>
+                                                      </div>
+                                                      :
+                                                      <>
+                                                            <span
+                                                                  className="flex-row cart-list-item__head"
+                                                            >
+                                                                  <img src={icon.box} alt="" />
+                                                                  {t('Mer_de.services')}
+                                                            </span>
+                                                            <div className="flex-row cart-list-item__title">
+                                                                  <span>{t('Mer_de.services_name')}</span>
+                                                                  <span>{t('pr.quantity')}</span>
+                                                                  <span>{t('cart.unit_price')}</span>
+                                                                  <span>{t('pr.total')}</span>
+                                                                  <span>{t('cart.option')}</span>
+                                                            </div>
+                                                            <ul className="flex-column">
+                                                                  {
+                                                                        item.cart.filter((item: any) => item.is_type === 2).map((child: any) => (
+                                                                              <CartItem
+                                                                                    key={child.cart_id}
+                                                                                    cartItem={child}
+                                                                                    chooseOrg={chooseOrg}
+                                                                              />
+                                                                        ))
+                                                                  }
+                                                            </ul>
+                                                      </>
+                                          }
+                                          {/* products list */}
+                                          {
+                                                item.cart.filter((item: any) => item.is_type === 1).length === 0 ?
+                                                      <></>
+                                                      :
+                                                      <>
+                                                            <span
+                                                                  className="flex-row cart-list-item__head"
+                                                            >
+                                                                  <img src={icon.bag} alt="" />
+                                                                  {t('Mer_de.products')}
+                                                            </span>
+                                                            <ul className="flex-column">
+                                                                  {
+                                                                        item.cart.filter((item: any) => item.is_type === 1).map((child: any) => (
+                                                                              <CartItem
+                                                                                    key={child.cart_id}
+                                                                                    cartItem={child}
+                                                                                    chooseOrg={chooseOrg}
+                                                                              />
+                                                                        ))
+                                                                  }
+                                                            </ul>
+                                                      </>
+                                          }
+                                          {/* combos */}
+                                          {
+                                                item.cart.filter((item: any) => item.is_type === 3).length === 0 ?
+                                                      <></>
+                                                      :
+                                                      <>
+                                                            <span
+                                                                  className="flex-row cart-list-item__head"
+                                                            >
+                                                                  <img src={icon.bag} alt="" />
+                                                                  Combos
+                                                            </span>
+                                                            <ul className="flex-column">
+                                                                  {
+                                                                        item.cart.filter((item: any) => item.is_type === 3).map((child: any) => (
+                                                                              <CartItem
+                                                                                    key={child.cart_id}
+                                                                                    cartItem={child}
+                                                                                    chooseOrg={chooseOrg}
+                                                                              />
+                                                                        ))
+                                                                  }
+                                                            </ul>
+                                                      </>
+                                          }
                                     </div>
                               ))
                               :
@@ -143,7 +173,6 @@ function CartList(props:any) {
                                           }
                                     </ul>
                               </>
-
                   }
             </div>
       );
